aa62cde77fcecf4d351454fcab94b4ae649ab938
iamgamja <hmmthinking@naver.com>
2023-01-09 17:21:04 +0900

    db를 수정하는 기능 추가

--- src/util/db/getDb.ts
+++ src/util/db/getDb.ts
@@ -1,19 +1,10 @@
 import DbChannelNotFoundError from '../../error/DbChannelNotFoundError'
 import DbChannelIsNotTextChannelError from '../../error/DbChannelIsNotTextChannelError'
-import { ChannelType, Snowflake } from 'discord.js'
+import { ChannelType } from 'discord.js'
 import { bot } from '../../main'
-import { 던전쿨타임db, 도박쿨타임db, 유저정보db, 출첵쿨타임db } from '../../type/db'
-
-type dbName = '유저 정보' | '출첵 쿨타임' | '던전 쿨타임' | '도박 쿨타임'
-
-const messageId: { [name in dbName]: Snowflake } = {
-  '유저 정보': '1025653282254880829',
-  '출첵 쿨타임': '1030671119797207040',
-  '던전 쿨타임': '1028912965786796144',
-  '도박 쿨타임': '1036179914367447041',
-}
-
-type dbData<name extends dbName> = name extends '유저 정보' ? 유저정보db : name extends '출첵 쿨타임' ? 출첵쿨타임db : name extends '던전 쿨타임' ? 던전쿨타임db : 도박쿨타임db
+import { dbData } from '../../type/dbData'
+import { dbName } from '../../type/dbName'
+import { messageId } from '../../data/messageId'
 
 export async function getDbMessage(name: dbName) {
   const dbChannel = bot.channels.cache.get('1025653116441464842')
