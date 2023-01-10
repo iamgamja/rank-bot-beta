8bded3f923e197047cbb9be032ea913bf5266626
iamgamja <hmmthinking@naver.com>
2023-01-10 17:18:18 +0900

    add `add` function, rename calculate남은경험치/누적레벨

--- src/util/calculate남은경험치.ts
+++ src/util/calculate남은경험치.ts
@@ -2,14 +2,14 @@ import { GuildMember } from 'discord.js'
 import UserNotFoundError from '../error/UserNotFoundError'
 import getUserData from './db/get/getUserData'
 import isUser from './check/isUser'
-import calculate누적레벨 from './calculate누적레벨'
+import { calculate누적레벨ByUser } from './calculate누적레벨'
 
 export default async function calculate남은경험치(member: GuildMember) {
   if (!(await isUser(member))) throw new UserNotFoundError()
 
   const userData = await getUserData(member)
 
-  const 누적레벨 = await calculate누적레벨(member)
+  const 누적레벨 = await calculate누적레벨ByUser(member)
   const 목표경험치 = 2 ** (누적레벨 - 1) * 1000
   const 남은경험치 = 목표경험치 - userData.경험치
   return 남은경험치
--- src/util/calculate누적레벨.ts
+++ src/util/calculate누적레벨.ts
@@ -2,11 +2,9 @@ import { GuildMember } from 'discord.js'
 import UserNotFoundError from '../error/UserNotFoundError'
 import getUserData from './db/get/getUserData'
 import isUser from './check/isUser'
+import { 유저정보 } from '../type/db'
 
-export default async function calculate누적레벨(member: GuildMember) {
-  if (!(await isUser(member))) throw new UserNotFoundError()
-
-  const userData = await getUserData(member)
+export async function calculate누적레벨ByUserData(userData: 유저정보) {
   let tear = userData.티어
   let level = userData.레벨
   while (tear) {
@@ -15,4 +13,11 @@ export default async function calculate누적레벨(member: GuildMember) {
   }
 
   return level
+}
+
+export async function calculate누적레벨ByUser(member: GuildMember) {
+  if (!(await isUser(member))) throw new UserNotFoundError()
+
+  const userData = await getUserData(member)
+  return await calculate누적레벨ByUserData(userData)
 }
