7cb13b9df54c314502e929292af8b7163462da9e
iamgamja <hmmthinking@naver.com>
2023-01-13 17:11:02 +0900

    eval에 query 추가

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable unused-imports/no-unused-imports */
 import { ApplicationCommandOptionType, CommandInteraction } from 'discord.js'
 import { Discord, Slash, SlashOption } from 'discordx'
+import query from '../util/query'
 
 @Discord()
 export class Eval {
