0d95dac6700b897b5d73f7c24ff2be79f43658cd
iamgamja <hmmthinking@naver.com>
2023-01-15 15:54:09 +0900

    haee 커맨드 삭제, 날짜 함수 이름 변경 (haee => 날짜)

--- src/commands/날짜.ts
+++ src/commands/날짜.ts
@@ -4,7 +4,7 @@ import { Discord, Slash } from 'discordx'
 @Discord()
 export class 날짜 {
   @Slash({ description: '2020년 10월 17일을 0일로 계산해서 오늘 날짜를 확인합니다.', name: '날짜' })
-  async haee(interaction: CommandInteraction) {
+  async 날짜(interaction: CommandInteraction) {
     const diffms = new Date().getTime() - new Date('2020 10 17').getTime()
     const diffday = Math.floor(diffms / 1000 / 60 / 60 / 24)
     await interaction.reply(`오늘은 ${diffday}일 입니다.`)
